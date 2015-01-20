4f31dcb6d5db496aacb80a21794ceb9541d6e9d6
Shane Blatt <shane.blatt@yahoo.com>
2015-01-20 15:05:18 -0500

    JQuery fumbling

--- accordian.js
+++ accordian.js
@@ -1,1 +1,9 @@
 console.log("Work Accordian");
+
+// $('.box1').on('click', function (x) {
+// 	alert('hello');
+// });
+
+$('.container1').on('click', function () {
+	return $(this).removeClass('expand');
+});
