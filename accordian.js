6e5b5881691ef44a4f67c057fb7b1786f94e70f3
Shane Blatt <shane.blatt@yahoo.com>
2015-01-20 17:43:26 -0500

    JQuery fumbling

--- accordian.js
+++ accordian.js
@@ -3,7 +3,24 @@ console.log("Work Accordian");
 // $('.box1').on('click', function (x) {
 // 	alert('hello');
 // });
-
 $('.container1').on('click', function () {
-	return $(this).removeClass('expand');
+	$('.content1').addClass('open1');
 });
+
+$('.container2').on('click', function () {
+	$('.content1').removeClass('open1');
+	$('.content2').addClass('open2');
+});
+
+$('.container3').on('click', function () {
+	$('.content2').removeClass('open2');
+	$('.content3').addClass('open3');
+	
+});
+
+// Set height of div to 0;
+// overflow: hidden ... p
+
+// target container in jquery
+
+
